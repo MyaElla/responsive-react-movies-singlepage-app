73dfbc64ecf02663792997cd224215c9ebb2f275
Mihaela Ciobanu <mihaela.ciobanu03@gmail.com>
2019-08-01 16:02:59 +0100

    styled rating text

--- src/pages/RatingStars.js
+++ src/pages/RatingStars.js
@@ -2,9 +2,9 @@ import React from "react";
 import StyledScore from '../components/StyledScore'
 
 const RatingStars = ({ rating }) => {
-    const score = (rating.average === null) ? 
-        <StyledScore italic subtle>No rating yet</StyledScore> 
-        : 
+    const score = (rating.average === null) ?
+        <StyledScore italic subtle>No rating yet</StyledScore>
+        :
         <StyledScore>{rating.average}</StyledScore>
 
     return (
